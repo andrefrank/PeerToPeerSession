595059058767cbba5ada4cb1e51940e1b269342b
Andre Frank <andrefrank.mail@t-online.de>
2022-02-19 21:00:02 +0100

    Initial Commit

---
+++ PeerToPeerSession/PeerToPeerSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeerToPeerSessionApp.swift
+//  PeerToPeerSession
+//
+//  Created by Andre Frank on 19.02.22.
+//
+
+import SwiftUI
+
+@main
+struct PeerToPeerSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
